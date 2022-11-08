19207c363e3baaa2e71432c284890ab76f2051bd
software-design-jerkins <rick8963@gmail.com>
2022-11-08 11:20:47 +0800

    test code for grade converter

---
+++ src/test/java/GradeConverterTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GradeConverterTest {
+
+    GradeConverter converter = new GradeConverter();
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void convert() {
+    }
+
+    @Test
+    public void testConverter1()
+    {
+        String expected = "A";
+        String actual = converter.convert(100);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConverter2()
+    {
+        String expected = "A";
+        String actual = converter.convert(90);
+        assertEquals(expected, actual);
+    }
+}
